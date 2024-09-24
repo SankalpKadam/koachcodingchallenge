2793647c41cc7e237e62241fcd266887989ac795
SankalpKadam <sankalptp07@gmail.com>
2024-09-24 09:01:06 -0500

    Basic user profile card completed

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import '../UserProfile/index.css'
 const UserProfile = () => {
-  return (
-    <div className = "userProfile">
-      
-    </div>
-  )
+    return (
+        <div className="userProfile">
+            <p className='userName'>Leanne Graham</p>
+            <p className='userEmail'>Sincere@april.biz</p>
+            <p className='userPhone'>1-770-736-8031</p>
+            <p className='userAddress'>Kulas Light, Apt. 556, Gwenborough, 92998-3874</p>
+        </div>
+    )
 }
 
 export default UserProfile
