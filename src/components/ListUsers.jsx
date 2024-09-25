ce38f5c3abeec48a0db3d265d6c844d38b6ed3ee
SankalpKadam <sankalptp07@gmail.com>
2024-09-24 22:14:58 -0500

    Component for displaying list of uses

--- src/components/ListUsers.jsx
+++ src/components/ListUsers.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import UserCard from './UserCard'
+import './index.css'
 const ListUsers = () => {
-  return (
-    <div>
-      <Link to={'/users'}>Users</Link>
-    </div>
-  )
+
+    return (
+        <div className='listUsers'>
+            <p>
+                Users
+            </p>
+            <div className='list'>
+
+                <UserCard id={1}/>
+                <UserCard id={1}/>
+                <UserCard id={1}/>
+                <UserCard id={1}/>
+                <UserCard id={1}/>
+                <UserCard id={1}/>
+                <UserCard id={1}/>
+                <UserCard id={1}/>
+                <UserCard id={1}/>
+                <UserCard id={1}/>
+                <UserCard id={1}/>
+                <UserCard id={1}/>
+                <UserCard id={1}/>
+                <UserCard id={1}/>
+                
+            </div>
+        </div>
+    )
 }
 
 export default ListUsers
