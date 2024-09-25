e8a894bf087e90c6cb9f510f823fa6f7aa76a887
SankalpKadam <sankalptp07@gmail.com>
2024-09-24 22:15:40 -0500

    Update UserPage.jsx

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import UserProfile from './UserProfile/UserProfile'
 import UserActivities from './UserActivities/UserActivities'
 import './index.css'
+import { useParams } from 'react-router-dom'
 const UserPage = () => {
+    const {id} = useParams()
   return (
     <div className='userPage'>
-      <UserProfile/>
+      <UserProfile id={id}/>
       <UserActivities/>
     </div>
   )
