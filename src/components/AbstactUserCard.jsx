32a58e19fd4c1a47f5d65f71ed70f6233ccf6ce4
SankalpKadam <sankalptp07@gmail.com>
2024-09-24 22:52:27 -0500

    Loader component for user

---
+++ src/components/AbstactUserCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Skeleton from 'react-loading-skeleton'
+
+const AbstactUserCard = () => {
+  return (
+    Array(10).fill(0).map((item, index)=>(
+        <div className='userCard' key={index}>
+            <Skeleton className='userListId'/>
+            <Skeleton className='userListName'/>
+            <Skeleton className='userListBtn' />
+        </div>
+    ))
+  )
+}
+
+export default AbstactUserCard
