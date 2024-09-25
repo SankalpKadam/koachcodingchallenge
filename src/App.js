cd30402139e674b9e48ec3319d906e2a06153d0c
SankalpKadam <sankalptp07@gmail.com>
2024-09-24 19:53:15 -0500

    Setup react browser router

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import UserActivities from './components/UserPage/UserActivities/UserActivities'
 import UserProfile from './components/UserPage/UserProfile/UserProfile';
 import { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css'
+import UserPage from './components/UserPage/UserPage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListUsers from './components/ListUsers';
 function App() {
 
   return (
     <div className="App">
       <SkeletonTheme baseColor="#e1e2e2" highlightColor="#bcbcbc">
-
-        <UserProfile email={"sankalptop"} phone={1234567} address={"Hello world"} />
-        <UserActivities />
+        <BrowserRouter>
+          {/* <UserProfile email={"sankalptop"} phone={1234567} address={"Hello world"} />
+        <UserActivities /> */}
+        <Routes path='/'>
+            <Route index element={<ListUsers/>}/>
+            <Route path='users/:id'/>
+          {/* <UserPage /> */}
+        </Routes>
+        </BrowserRouter>
       </SkeletonTheme>
     </div>
   );
